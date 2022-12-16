83047a43bdd59890166d37eb1a81332b6009acf6
5hpik <maxim.loboda2013@yandex.ru>
2022-12-16 11:11:49 +0700

    Removed garbage and changed inner logic a bit

---
+++ src/com/pipaengine/experimental/Main.java
@@ -0,0 +1,8 @@
+package com.pipaengine.legacy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Game();
+    }
+}
---
+++ src/com/pipaengine/experimental/Settings.java
@@ -0,0 +1,47 @@
+package com.pipaengine.legacy;
+
+import java.io.Serializable;
+
+public class Settings implements Serializable {
+    private boolean fullscreen = true;
+    private int resX = 640, resY = 480, renderResX = 640, renderResY = 480;
+
+    public boolean isFullscreen() {
+        return fullscreen;
+    }
+
+    public void setFullscreen(boolean fullscreen) {
+        this.fullscreen = fullscreen;
+    }
+
+    public int getResX() {
+        return resX;
+    }
+
+    public void setResX(int resX) {
+        this.resX = resX;
+    }
+
+    public int getResY() {
+        return resY;
+    }
+
+    public void setResY(int resY) {
+        this.resY = resY;
+    }
+
+    public int getRenderResX() {
+        return renderResX;
+    }
+
+    public void setRenderResX(int renderResX) {this.renderResX = renderResX;
+    }
+
+    public int getRenderResY() {
+        return renderResY;
+    }
+
+    public void setRenderResY(int renderResY) {
+        this.renderResY = renderResY;
+    }
+}
