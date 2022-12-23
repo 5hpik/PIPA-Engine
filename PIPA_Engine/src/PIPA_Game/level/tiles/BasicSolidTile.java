d447640dc5e4c7a1e9c912c62e66c265468c6f05
Miroxq <opium0601@gmail.com>
2022-12-24 04:18:50 +0700

    Collision

---
+++ PIPA_Engine/src/PIPA_Game/level/tiles/BasicSolidTile.java
@@ -0,0 +1,9 @@
+package PIPA_Game.level.tiles;
+
+public class BasicSolidTile extends BasicTile {
+
+    public BasicSolidTile(int id, int x, int y, int tileColour) {
+        super(id, x, y, tileColour);
+        this.solid=true;
+    }
+}
--- PIPA_Engine/src/PIPA_Game/level/tiles/Tile.java
+++ PIPA_Engine/src/PIPA_Game/level/tiles/Tile.java
@@ -4,12 +4,14 @@ import PIPA_Game.gfx.Colours;
 import PIPA_Game.gfx.Screen;
 import PIPA_Game.level.Level;
 
+import javax.swing.plaf.basic.BasicBorders;
+
 public abstract class Tile {
 
     public static final Tile[] tiles = new Tile[256];
-    public static final Tile VOID = new BasicTile(0, 0, 0, Colours.get(000, -1,
+    public static final Tile VOID = new BasicSolidTile(0, 0, 0, Colours.get(000, -1,
             -1, -1));
-    public static final Tile STONE = new BasicTile(1, 1, 0, Colours.get(-1,
+    public static final Tile STONE = new BasicSolidTile(1, 1, 0, Colours.get(-1,
             333, -1, -1));
     public static final Tile GRASS = new BasicTile(2, 2, 0, Colours.get(-1,
             131, 141, -1));
